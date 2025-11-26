27ffd98399146af4e0bfac8e3f1fdc4873e10935
larry <kaihsun_chen@9splay.com>
2025-11-26 13:37:54 +0800

    60. Rendering Content Conditionally  61. Accepting & Passing Functions as Values  62. The "initState" Method 05 initState
    InitState、傳參(quiz->start_screen->按鈕->SwitchScreen()->question_screen)

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -16,7 +18,7 @@ class StartScreen extends StatelessWidget {
           ),
           SizedBox(height: 80),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             icon: Icon(Icons.arrow_right_alt),
             label: Text('Start Quiz'),
